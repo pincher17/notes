06308d9dc596dbfa9f2ff163b88ea6e28a42d06a
andrei <pinchers17@mail.ru>
2022-07-10 22:28:21 +0300

    add rect-roter-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.scss';
 import CreateNotes from './components/CreateNotes/CreateNotes.jsx';
 import Notes from './components/Notes/Notes.jsx';
 import SelectTags2 from './components/Filter/SelectTags2.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div className='App'>
     <CreateNotes />
     <SelectTags2 />
     <Notes />
     </div>
+    </BrowserRouter>
   );
 }
 
